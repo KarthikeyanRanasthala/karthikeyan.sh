dedcfe350b10f93e32aaee0b9d8e974e06085ea1
Karthikeyan Ranasthala <karthikeyan.ranasthala@gmail.com>
2020-08-14 22:39:48 +0530

    Add Custom404

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+const Custom404 = (): JSX.Element => (
+  <main className="flex text-lg justify-center items-center mt-40 md:mt-64">
+    <p className="border-r-4 border-green-500 p-4 font-bold">404</p>
+    <p className="p-4">This page could not be found.</p>
+  </main>
+);
+
+export default Custom404;
