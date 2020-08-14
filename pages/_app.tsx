f155815c2160464b41227cc3924948fae597cf9b
Karthikeyan Ranasthala <karthikeyan.ranasthala@gmail.com>
2020-08-14 22:14:56 +0530

    Add NavBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,24 @@ import { AppProps } from 'next/app';
 
 import 'src/styles/index.css';
 
-const CustomApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
-);
+import NavBar from 'src/components/common/NavBar';
+
+const CustomApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <style jsx>
+        {`
+          header {
+            opacity: 0.9;
+          }
+        `}
+      </style>
+      <header className="sticky top-0 max-w-screen-xl m-auto px-4">
+        <NavBar />
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default CustomApp;
---
+++ src/components/common/NavBar.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const links = [
+  {
+    title: 'Home',
+    href: '/',
+  },
+];
+
+const NavBar: React.FC<unknown> = () => {
+  const router = useRouter();
+  return (
+    <>
+      <style jsx>
+        {`
+          .nav-link {
+            transition: border-color 0.2s ease;
+          }
+          .nav-link:not(.active):hover {
+            @apply border-dashed border-green-500;
+          }
+          .nav-link:not(.active) > a {
+            transition: color 0.2s ease-in-out;
+          }
+          .nav-link:not(.active) > a:hover {
+            @apply text-green-500;
+          }
+        `}
+      </style>
+      <nav className="flex justify-end bg-primary-dark">
+        <ul className="flex my-4 md:my-8">
+          {links.map((link) => (
+            <li
+              key={link.title}
+              className={`ml-8 border-4 rounded-lg nav-link ${
+                router.asPath === link.href
+                  ? 'border-green-500 active'
+                  : 'border-primary-dark'
+              }`}
+            >
+              <Link href={link.href}>
+                <a className="px-4 py-2 block text-white text-lg font-bold">
+                  {link.title}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
