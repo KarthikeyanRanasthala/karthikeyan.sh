15095d86f8b9104fbb6058ccefb2b700d99f246e
Karthikeyan Ranasthala <karthikeyan.ranasthala@gmail.com>
2020-08-14 21:33:13 +0530

    Extend Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,26 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   purge: ['./src/components/**/*.{jsx,tsx}', './pages/**/*.{jsx,tsx}'],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          dark: '#121212',
+        },
+      },
+    },
+  },
   variants: {},
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, config }) {
+      addBase({
+        h1: { fontSize: config('theme.fontSize.5xl') },
+        h2: { fontSize: config('theme.fontSize.4xl') },
+        h3: { fontSize: config('theme.fontSize.3xl') },
+        h4: { fontSize: config('theme.fontSize.2xl') },
+        h5: { fontSize: config('theme.fontSize.xl') },
+      });
+    }),
+  ],
 };
