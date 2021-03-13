bbce30d13546f025503420aa27ea22418cf8c7a8
Karthikeyan Ranasthala <karthikeyan.ranasthala@gmail.com>
2021-03-13 22:27:47 +0530

    Add Custom Server

---
+++ server.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const next = require('next');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('*', (req, res) => {
+    if (req.headers['x-forwarded-proto'] === 'https') {
+      handle(req, res);
+    } else {
+      res.redirect(301, `https://${req.hostname}${req.url}`);
+    }
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
